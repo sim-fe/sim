b05f500575abce16fcec76427580586416431523
linjingming <linjingming1201@gmail.com>
2018-12-28 14:44:11 +0800

    删除无用代码

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,19 @@
 const MarkdownItContainer = require('markdown-it-container')
-const md = require('markdown-it')();
-const striptags = require('./strip-tags')
-const wrapCustomClass = function (render) {
-    return function (...args) {
-      return render(...args)
-        .replace('<code class="', '<code class="hljs ')
-        .replace('<code>', '<code class="hljs">')
+const md = require('markdown-it')()
+const wrapCustomClass = function(render) {
+    return function(...args) {
+        return render(...args)
+            .replace('<code class="', '<code class="hljs ')
+            .replace('<code>', '<code class="hljs">')
     }
-  }
-  
-  /**
-   * Format HTML string
-   */
-const convertHtml = function (str) {
-    return str.replace(/(&#x)(\w{4});/gi, $0 => String.fromCharCode(parseInt(encodeURIComponent($0).replace(/(%26%23x)(\w{4})(%3B)/g, '$2'), 16)))
-  }
+}
+
 const vueMarkdown = {
     raw: true,
     preventExtract: true,
     preprocess: (MarkdownIt, source) => {
-        MarkdownIt.renderer.rules.table_open = function () {
-        return '<table class="table">'
+        MarkdownIt.renderer.rules.table_open = function() {
+            return '<table class="table">'
         }
         MarkdownIt.renderer.rules.fence = wrapCustomClass(MarkdownIt.renderer.rules.fence)
 
@@ -28,7 +21,7 @@ const vueMarkdown = {
         //  但是markdown-it 有个bug fence整合attr的时候直接加载class数组上而不是class的值上
         //  markdown-it\lib\renderer.js 71行 这么修改可以修复bug
         //  tmpAttrs[i] += ' ' + options.langPrefix + langName; --> tmpAttrs[i][1] += ' ' + options.langPrefix + langName;
-        // const fence = MarkdownIt.renderer.rules.fence 
+        // const fence = MarkdownIt.renderer.rules.fence
         // MarkdownIt.renderer.rules.fence = function(...args){
         //   args[0][args[1]].attrJoin('class', 'hljs')
         //   var a = fence(...args)
@@ -37,31 +30,33 @@ const vueMarkdown = {
 
         // ```code`` 给这种样式加个class code_inline
         const code_inline = MarkdownIt.renderer.rules.code_inline
-        MarkdownIt.renderer.rules.code_inline = function(...args){
-        args[0][args[1]].attrJoin('class', 'code_inline')
-        return code_inline(...args)
+        MarkdownIt.renderer.rules.code_inline = function(...args) {
+            args[0][args[1]].attrJoin('class', 'code_inline')
+            return code_inline(...args)
         }
         return source
     },
     use: [
-        [MarkdownItContainer, 'demo', {
-        validate: params => params.trim().match(/^demo\s*(.*)$/),
-        render: function(tokens, idx) {
-            var m = tokens[idx].info.trim().match(/^demo\s*(.*)$/);
-            if (tokens[idx].nesting === 1) {
-                var description = (m && m.length > 1) ? m[1] : '';
-                var content = tokens[idx + 1].content;
-                var descriptionHTML = description
-                ? md.render(description)
-                : '';
-                return `<demo-block>
+        [
+            MarkdownItContainer,
+            'demo',
+            {
+                validate: params => params.trim().match(/^demo\s*(.*)$/),
+                render: function(tokens, idx) {
+                    var m = tokens[idx].info.trim().match(/^demo\s*(.*)$/)
+                    if (tokens[idx].nesting === 1) {
+                        var description = m && m.length > 1 ? m[1] : ''
+                        var content = tokens[idx + 1].content
+                        var descriptionHTML = description ? md.render(description) : ''
+                        return `<demo-block>
                         <div class="source" slot="source">${content}</div>
                         ${descriptionHTML}
-                        <div class="highlight" slot="highlight">`;
+                        <div class="highlight" slot="highlight">`
+                    }
+                    return '</div></demo-block>\n'
+                }
             }
-            return '</div></demo-block>\n';
-        }
-        }],
+        ],
         [MarkdownItContainer, 'tip'],
         [MarkdownItContainer, 'warning']
     ]
@@ -74,29 +69,27 @@ module.exports = {
             entry: 'examples/main.js',
             template: 'examples/index.html',
             filename: 'index.html'
-          }
+        }
     },
     chainWebpack: config => {
         config.module
-        .rule("md")
-        .test(/\.md/)
-        .use("vue-loader")
-        .loader("vue-loader")
-        .end()
-        .use("vue-markdown-loader")
-        .loader("vue-markdown-loader/lib/markdown-compiler")
-        .options(vueMarkdown)
+            .rule('md')
+            .test(/\.md/)
+            .use('vue-loader')
+            .loader('vue-loader')
+            .end()
+            .use('vue-markdown-loader')
+            .loader('vue-markdown-loader/lib/markdown-compiler')
+            .options(vueMarkdown)
     },
     css: {
         sourceMap: true,
-        loaderOptions: {
-         
-        }
+        loaderOptions: {}
     },
     devServer: {
         overlay: {
-          warnings: true,
-          errors: false
+            warnings: true,
+            errors: false
         }
     }
 }
