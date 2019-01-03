05bab749989a54ec68b94434da4e0e2bcaa8fd4d
linjingming <linjingming1201@gmail.com>
2019-01-03 09:46:06 +0800

    git 链接修改

--- examples/pages/index.vue
+++ examples/pages/index.vue
@@ -7,7 +7,7 @@
     <div class="separate-panel">
       <div class="about">
         <router-link :to="{name: 'giud'}">开始使用</router-link>
-        <a target="_blank" class="btn-github" href="https://github.com/VV-UI/VV-UI">GitHub</a>
+        <a target="_blank" class="btn-github" href="https://github.com/sim-fe/sim">GitHub</a>
       </div>
     </div>
   </div>
