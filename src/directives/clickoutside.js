869ba788eb220e14b7037ee4c5048195bcdb6bf8
linjingming <linjingming1201@gmail.com>
2019-01-04 14:36:35 +0800

    加update

--- src/directives/clickoutside.js
+++ src/directives/clickoutside.js
@@ -1,19 +1,33 @@
-let nodeList = [];
-document.addEventListener('mouseup', e => {
-  nodeList.forEach(node => {
-    node._handler(e);
-  });
-}, false);
+let nodeList = []
+document.addEventListener(
+	'mouseup',
+	e => {
+		nodeList.forEach(node => {
+			node._handler(e)
+		})
+	},
+	false
+)
 export default {
-    bind (el, binding) {
-      el._handler = e => {
-        if (!el.contains(e.target) && binding.expression) {
-          binding.value(e)
-        }
-      }
-      nodeList.push(el);
-    },
-    unbind () {
-      nodeList = [];
-    }
-  }
+	bind(el, binding) {
+		el._handler = e => {
+			if (!el.contains(e.target) && binding.expression) {
+				binding.value(e)
+			}
+		}
+		nodeList.push(el)
+	},
+	update(el, binding) {
+		el._handler = e => {
+			if (!el.contains(e.target) && binding.expression) {
+				binding.value(e)
+			}
+		}
+		if (nodeList.indexOf(el) === -1) {
+			nodeList.push(el)
+		}
+	},
+	unbind() {
+		nodeList = []
+	}
+}
