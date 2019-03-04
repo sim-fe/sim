636515b8363cd1cb12c60238b4dc0aaa8bf3ca4f
linjingming <linjingming1201@gmail.com>
2019-03-04 15:14:18 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-import Button from './components/button'
-import Modal from './components/modal'
-import ButtonGroup from './components/button-group'
-import Input from './components/input'
-import Progress from './components/progress'
-import Option from './components/option'
-import Select from './components/select'
-import Message from './components/message'
+import Button from './components/button';
+import Modal from './components/modal';
+import ButtonGroup from './components/button-group';
+import Input from './components/input';
+import Progress from './components/progress';
+import Option from './components/option';
+import Select from './components/select';
+import Message from './components/message';
 
-import locale from './locale'
+import locale from './locale';
 
-const components = [Button, Modal, ButtonGroup, Input, Select, Progress, Option, Message]
+const components = [Button, Modal, ButtonGroup, Input, Select, Progress, Option, Message];
 
 const install = function(Vue, opts = {}) {
-    if (install.installed) return
-    locale.use(opts.locale)
-    locale.i18n(opts.i18n)
-    components.map(component => Vue.component(component.name, component))
+    if (install.installed) return;
+    locale.use(opts.locale);
+    locale.i18n(opts.i18n);
+    components.map(component => Vue.component(component.name, component));
 
-    Vue.prototype.$Message = Message
-}
+    Vue.prototype.$Message = Message;
+};
 
 if (typeof window !== 'undefined' && window.Vue) {
-    install(window.Vue)
+    install(window.Vue);
 }
 
 export default {
     install,
     ...components
-}
+};
