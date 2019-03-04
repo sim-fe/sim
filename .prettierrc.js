bf03e9b7c9c1e9cdb557d1e807a359db0dbf276a
linjingming <linjingming1201@gmail.com>
2019-03-04 15:13:18 +0800

    prettier 配置独立

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "singleQuote": true, // 使用单引号
+    "printWidth": 100, // 换行字符串阈值
+    "semi": true,  // 句末加分号
+    "useTabs": false, //
+    "tabWidth": 4, // 4空格
+    "bracketSpacing": false, // 对象 数组加空格
+    "endOfLine": "lf" // 结尾换行为 LF（Linux和macOS上很常见 ） CRLF（回车加换行，window比较常见）
+};
